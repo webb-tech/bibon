754d591111da6ad10904975e89bb3263332d9682
alexanderiksson <Alexerik69@gmail.com>
2026-02-06 09:52:44 +0100

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
                                         src="https://images.unsplash.com/photo-1528605248644-14dd04022da1?auto=format&fit=crop&w=800&q=80"
                                         alt="Restaurant interior"
                                         fill
-                                        className="object-cover rounded"
+                                        className="object-cover"
                                     />
                                 </div>
                                 <div className="flex flex-col justify-center items-center text-center px-4">
@@ -38,7 +38,7 @@ export default function Home() {
                                         src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=800&q=80"
                                         alt="Fish dish"
                                         fill
-                                        className="object-cover rounded"
+                                        className="object-cover"
                                     />
                                 </div>
                                 <div className="flex flex-col justify-center items-center text-center px-4">
@@ -58,7 +58,7 @@ export default function Home() {
                                         src="https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?auto=format&fit=crop&w=800&q=80"
                                         alt="Menu on table"
                                         fill
-                                        className="object-cover rounded"
+                                        className="object-cover"
                                     />
                                 </div>
                                 <div className="flex flex-col justify-center items-center text-center px-4">
@@ -74,9 +74,9 @@ export default function Home() {
                 </div>
             </section>
 
-            <section className="py-16 bg-(--foreground)">
+            <section className="py-16">
                 <div className="container px-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <div className="flex flex-col gap-4 p-6 md:p-8 min-h-112.5 bg-(--background) text-(--foreground) rounded">
+                    <div className="flex flex-col gap-4 p-6 md:p-8 min-h-112.5 bg-(--yellow) text-(--background)">
                         <h2 className="text-center text-xl md:text-2xl mb-8">Öppettider</h2>
                         <div className="flex justify-between items-center border-b border-dotted">
                             <span>Måndag & Tisdag</span>
@@ -99,7 +99,7 @@ export default function Home() {
                             <span>12:00 - 23:00</span>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center gap-4 p-6 md:p-8 min-h-112.5 bg-(--background) text-(--foreground) rounded">
+                    <div className="flex flex-col items-center gap-4 p-6 md:p-8 min-h-112.5 bg-(--yellow) text-(--background)">
                         <h2 className="text-center text-xl md:text-2xl mb-8">Vår meny</h2>
                         <p className="text-sm md:text-base max-w-md text-center mb-4">
                             På Tennstopet hyllar vi svenska mattraditioner med tidlösa klassiker.
@@ -119,7 +119,6 @@ export default function Home() {
             </section>
 
             <ImageGallery
-                className=""
                 images={[
                     {
                         url: "https://images.unsplash.com/photo-1528605248644-14dd04022da1?auto=format&fit=crop&w=1200&q=80",
@@ -144,8 +143,8 @@ export default function Home() {
                 ]}
             />
 
-            <section className="py-16 bg-(--foreground) text-black">
-                <div className="container px-4 grid gap-16 text-center text-sm md:text-base grid-cols-1 md:grid-cols-4">
+            <section className="py-4">
+                <div className="container px-4 grid gap-16 text-center text-sm md:text-base grid-cols-1 md:grid-cols-4 border-y py-12">
                     <div className="flex flex-col gap-3 items-center px-4 md:px-0">
                         <h2 className="text-xl">Dagsaktuella bokningar</h2>
                         <p>Telefon: 08 32 25 18</p>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section
-            id="hero"
-            style={{
-                backgroundImage:
-                    "url(https://images.unsplash.com/photo-1481833761820-0509d3217039)",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                backgroundBlendMode: "multiply",
-            }}
-        >
-            <div className="container flex flex-col justify-center items-center gap-8 py-44 text-center">
-                <h1 className="text-5xl lg:text-6xl">
-                    Där generationer möts,
-                    <br /> trivs, äter och dricker gott.
-                </h1>
+        <section id="hero">
+            <div
+                className="container flex flex-col justify-center items-center gap-12 py-24 text-center"
+                style={{
+                    backgroundImage:
+                        "url(https://images.unsplash.com/photo-1481833761820-0509d3217039)",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    backgroundBlendMode: "multiply",
+                }}
+            >
+                <Image src="/logo-gray.png" alt="Logo" width={350} height={100} />
 
-                <div className="flex gap-8">
+                <p className="text-xl">
+                    &quot;Där generationer möts,
+                    <br /> trivs, äter och dricker gott.&quot;
+                </p>
+
+                <div className="flex flex-col gap-4">
                     <Link href="/" className="btn-primary">
                         Boka bord
                     </Link>
